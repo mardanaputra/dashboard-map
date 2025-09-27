8cb2ac243df51dba91a9601d57e896a8695a5365
Suniaramana Mardana Putra <mardanaputra@gmail.com>
2025-09-28 00:07:31 +0800

    Fix: import App.css case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // File: App.jsx
 import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import "./app.css";
+import "./App.css";
 import Navbar from "./components/Navbar";
 import DashboardContent from "./components/DashboardContent";
 import Footer from "./components/Footer";
